ee6b180d65b1e05e2cb7702c653a82c5b0948569
Vincent Goey <vincent.goey1003@gmail.com>
2020-02-09 14:13:04 +0800

    init project

---
+++ FruitDiary/FruitDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FruitDiary
+//
+//  Created by Kai Xuan on 09/02/2020.
+//  Copyright © 2020 Kai Xuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
